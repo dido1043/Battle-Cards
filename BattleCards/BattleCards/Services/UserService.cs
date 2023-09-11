d7e3b1e44c19c0cd34c162ed375dd1b0232ff5a4
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2023-09-11 18:35:57 +0300

    Up

--- BattleCards/BattleCards/Services/UserService.cs
+++ BattleCards/BattleCards/Services/UserService.cs
@@ -1,4 +1,6 @@
 ﻿using BattleCards.Data;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Program.Services
 {
@@ -30,20 +32,19 @@ namespace Program.Services
         }
 
         
-        public static string SHA512(string input)
+        public static string ComputeHash(string input)
         {
             var bytes = System.Text.Encoding.UTF8.GetBytes(input);
-            using (var hash = System.Security.Cryptography.SHA512.Create())
-            {
-                var hashedInputBytes = hash.ComputeHash(bytes);
+            using var hash = SHA512.Create();
+            
+            var hashedInputBytes = hash.ComputeHash(bytes);
 
-                // Convert to text
-                // StringBuilder Capacity is 128, because 512 bits / 8 bits in byte * 2 symbols for byte 
-                var hashedInputStringBuilder = new System.Text.StringBuilder(128);
-                foreach (var b in hashedInputBytes)
-                    hashedInputStringBuilder.Append(b.ToString("X2"));
-                return hashedInputStringBuilder.ToString();
-            }
+            // Convert to text
+            // StringBuilder Capacity is 128, because 512 bits / 8 bits in byte * 2 symbols for byte 
+            var hashedInputStringBuilder = new StringBuilder(128);
+            foreach (var b in hashedInputBytes)
+                hashedInputStringBuilder.Append(b.ToString("X2"));
+            return hashedInputStringBuilder.ToString();      
         }
     }
 }
